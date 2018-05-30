efef085f518ba9110847f0e4b2b51356d0f8e69b
Dezmon <chernolichenko.ai@gmail.com>
2018-05-30 20:21:29 +0300

    pre-last-push

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items: 1,
+  	autoplay: true,
+  	stopOnHover : true,
+  	loop: true,
+  	dots: false,
+  	nav: true,
+  });
+});
