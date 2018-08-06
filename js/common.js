3047c41797a4ff4dec599b999f914fdb3d2456dd
Dezmon <chernolichenko.ai@gmail.com>
2018-08-06 15:44:18 +0300

    fix bugs, add tabs

--- js/common.js
+++ js/common.js
@@ -7,4 +7,21 @@ $(document).ready(function(){
   	dots: false,
   	nav: true,
   });
+
+  function commonSwitchTab (selector) {
+    var getAttr = $(this).attr('data-tabControl');
+    $(selector + ' [data-tabControl]').removeClass('active')
+    $(this).addClass('active')
+    $(selector).attr('data-tab', '')
+    $(selector + '[data-tab]').attr('data-tab', getAttr)
+    $(selector + ' [data-tabContent]').removeClass('active');
+    $(selector + ' [data-tabContent="'+ getAttr + '"]').addClass('active')
+	}
+	function tabCall(selector) {
+	    $(selector + ' [data-tabControl]').on('click', function () {
+	        commonSwitchTab.call(this, selector);
+	    });
+	}
+
+	tabCall('.tabs__js')
 });
